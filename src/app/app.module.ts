45e86b890ee3f748889d821cfcf3c7ddf1a2300e
Tonypsilon <tony.schwedek@posteo.de>
2023-07-15 14:41:33 +0200

    #6 Style change password component. Add general margin-right to all form fields.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { NavigationComponent } from './navigation/navigation.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSelectModule } from '@angular/material/select';
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule} from "@angular/material/form-field";
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -58,6 +59,7 @@ import { ChangePasswordFormComponent } from './user/admin/change-password-form/c
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
+    MatFormFieldModule,
     MatCardModule,
     MatMenuModule,
     MatButtonModule,
