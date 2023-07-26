74ea3aca9dc7c4fca8b6c619ad5bfb46e4fba5ed
Tonypsilon <tony.schwedek@posteo.de>
2023-07-26 21:38:10 +0200

    #9 Show business message in case of HTTP 400, 401 or 403 error response.

--- src/app/error-handler.interceptor.ts
+++ src/app/error-handler.interceptor.ts
@@ -8,11 +8,13 @@ import {
 } from '@angular/common/http';
 import { Observable, catchError, throwError } from 'rxjs';
 import { Router } from '@angular/router';
+import {MessageService} from "./messages/message.service";
 
 @Injectable()
 export class ErrorHandlerInterceptor implements HttpInterceptor {
 
-  constructor(private router: Router) {}
+  constructor(private router: Router,
+              private messageService: MessageService) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     return next.handle(request).pipe(
@@ -22,12 +24,15 @@ export class ErrorHandlerInterceptor implements HttpInterceptor {
 
   handleErrorResponse(resp: HttpErrorResponse) {
     if(resp.status === 401 || resp.status === 403) {
+      this.messageService.error('Sie sind nicht authorisiert, um die gewünschte Aktion durchzuführen!');
       this.router.navigate(['/login']);
       return throwError(() => new Error('Nicht authorisiert!'));
     }
     if(resp.status === 400) {
+      this.messageService.error(resp.error.message);
       return throwError(() => new Error('Bad Request!'));
     }
+    this.messageService.error('Ein unbekanter Fehler ist aufgetreten.')
     return throwError(() => new Error('Unbekannter Fehler!'));
   }
 }
